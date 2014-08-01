c25e8f45590be93c30c0fe6a77a9a9d45e0cefee
tomyeh <tomyeh@potix.com>
2014-08-01 12:43:16 +0800

    unable to use source_maps later than 0.9.4 Need some effort to pull the changes from html5lib. Not sure if it is worth

--- lib/src/list_proxy.dart
+++ lib/src/list_proxy.dart
@@ -5,6 +5,7 @@
 library list_proxy;
 
 import 'dart:collection';
+import 'dart:math' show Random;
 
 // TOOD(jmesserly): this needs to be removed, but fixing NodeList is tricky.
 class ListProxy<E> extends IterableBase<E> implements List<E> {
